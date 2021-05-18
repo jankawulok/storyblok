4b53fad33dbb6661cc5302f0f51a5a851a85e10b
Jan Kawulok <jan@kawulok.com.pl>
2021-05-18 09:40:42 +0200

    feat: add storyblok-vue package

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,4 +1,8 @@
 import { integrationPlugin } from '@vue-storefront/core'
+import Vue from 'vue'
+import StoryblokVue from 'storyblok-vue';
+
+Vue.use(StoryblokVue);
 
 export default integrationPlugin(({ integration }) => {
   integration.configure('sb', { ...<%= serialize(options) %> })
